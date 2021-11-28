798549bc5b150a0649e7b01103abf3ec88a4c1b8
pkaehler <philipp.kaehler@googlemail.com>
2021-11-28 13:05:57 +0100

    sqlalchemy dependencies fixed

--- data_collector/consumer/consumer/database/connector.py
+++ data_collector/consumer/consumer/database/connector.py
@@ -1,5 +1,22 @@
-class LocalDB():
-    def __init__(self, config):
-        self.host = 'localhost'
+from sqlalchemy import create_engine
 
-# continue
+
+def get_connection_string(host: str = "localhost",
+                          port: int = 5432,
+                          db: str = "",
+                          user: str = None,
+                          password: str = None):
+    db_type = "postgresql"
+    if user and password:
+        return f"{db_type}://{user}:{password}@{host}:{port}/{db}"
+    else:
+        return f"{db_type}://{host}:{port}/{db}"
+
+
+def get_engine(connection_string):
+    print(connection_string)
+    engine = create_engine(connection_string)
+    return engine
+
+
+test = get_engine(get_connection_string(db="angstgegner"))
